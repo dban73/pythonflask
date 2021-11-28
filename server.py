e55d885e4e7cf1c9ad790848007eb412c671267f
Dban73 <dban95@hotmail.com>
2021-11-28 18:29:10 -0400

    CORS

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 # Server Flask
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 UPLOAD_FOLDER = "E:/Dev/python/projects/env2/pythonflask/uploads/"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
